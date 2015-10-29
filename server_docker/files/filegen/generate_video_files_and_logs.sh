08ee3ba11bc478f936e659338e4af278ea78bf90
mgithubreq <mferdman@cs.stonybrook.edu>
2015-10-29 07:22:37 +0000

    cleanup

--- server_docker/files/filegen/generate_video_files_and_logs.sh
+++ server_docker/files/filegen/generate_video_files_and_logs.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash -xe
 
 PARAMS=$1
 VIDEOS=$2
-mkdir -p $VIDEOS
+mkdir -p "$VIDEOS"
 
 cd /filegen
 
-for paramfile in $PARAMS/*; do
-	BN=`basename "$paramfile"`
-	cp -f $paramfile filegen_param.conf
+for paramfile in "$PARAMS/"*; do
+	BN=$(basename "$paramfile")
+	cp "$paramfile" filegen_param.conf
 	./make_zipf
-	./gen_fileset $VIDEOS/full-$BN- ./video_files.txt
+	./gen_fileset "$VIDEOS/full-$BN-" ./video_files.txt
 done
